44541d1634c5f098d846acb754ae5e08094d87b7
Danil-Prog <dany.gavrik@mail.ru>
2023-01-25 22:14:24 +0700

    refactor Posts validate

--- src/main/java/com/rest/api/entity/Post.java
+++ src/main/java/com/rest/api/entity/Post.java
@@ -3,8 +3,7 @@ package com.rest.api.entity;
 import lombok.Data;
 
 import javax.persistence.*;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 @Table(name = "posts")
 @Entity
@@ -16,13 +15,13 @@ public class Post {
     private Long id;
 
     @Column(name = "title")
-    @Min(value = 8, message = "Минимальная длина загаловка 8 символов")
-    @Max(value = 64, message = "Максимальная длина загаловка 64 символа")
+    @Size(min = 8, message = "Минимальная длина загаловка 8 символов")
+    @Size(max = 64, message = "Максимальная длина загаловка 64 символа")
     private String title;
 
     @Column(name = "text")
-    @Min(value = 8, message = "Минимальная длина текста 10 символов")
-    @Max(value = 64, message = "Максимальная длина поста 256 символа")
+    @Size(min = 8, message = "Минимальная длина текста 10 символов")
+    @Size(max = 64, message = "Максимальная длина поста 256 символа")
     private String text;
 
 }
