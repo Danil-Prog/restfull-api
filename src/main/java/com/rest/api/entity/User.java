99f1cd5db41ff3b6647bd728f940356d59bf1cb9
Daniil Gavrilov <dany.gavrik@mail.ru>
2023-01-25 15:11:05 +0700

    change field validation

--- src/main/java/com/rest/api/entity/User.java
+++ src/main/java/com/rest/api/entity/User.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Table(name = "user")
@@ -19,15 +18,13 @@ public class User {
     private Long id;
 
     @Column(name = "firstname")
-    @NotEmpty
     @Size(min = 2, message = "Минимальная длина имени 2 символа")
     @Size(max = 30, message = "Максимальная длина имени 30 символов")
     private String firstname;
 
     @Column(name = "lastname")
-    @NotEmpty
-    @Size(min = 2, message = "Минимальная длина имени 2 символа")
-    @Size(max = 30, message = "Максимальная длина имени 30 символов")
+    @Size(min = 2, message = "Минимальная длина фамилии 2 символа")
+    @Size(max = 30, message = "Максимальная длина фамилии 30 символов")
     private String lastname;
 
     @Column(name = "course")
